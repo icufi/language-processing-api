753ded724ecfad9046b081054f44f1831956dfc4
Max Girteit <icufishmg@gmail.com>
2021-06-13 10:45:18 -0600

    config set up

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,24 @@
 const path = require("path");
 const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { plugins } = require("./webpack.dev");
+module: {
+    rules: [
+            {
+        test: '/\.js$/',
+        exclude: /node_modules/,
+        loader: "babel-loader"
+            }
+    ]
+}
+
 module.exports = {
   entry: "./src/client/index.js",
 };
+
+plugins: [
+    new HtmlWebPackPlugin({
+        template: "./src/client/views/index.html",
+        filename: "./index.html",
+    }),
+]
