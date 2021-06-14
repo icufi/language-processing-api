b8ef2fc8cfe3dd80432d621bca1c888216da2976
Max Girteit <icufishmg@gmail.com>
2021-06-13 20:41:05 -0600

    everything working up, stopped at service workers config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,14 +1,20 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: "./src/client/index.js",
   mode: "production",
   output: {
-    libraryTarget: 'var',
-    library: 'Client'
-},
+    libraryTarget: "var",
+    library: "Client",
+  },
+  optimization: {
+    minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
   module: {
     rules: [
       {
@@ -18,11 +24,12 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
     ],
   },
   plugins: [
+    new MiniCssExtractPlugin({ filename: "[name].css" }),
     new HtmlWebPackPlugin({
       template: "./src/client/views/index.html",
       filename: "./index.html",
