acaeb734c489f3ef0780a2e8089464e2510aeb96
Max Girteit <icufishmg@gmail.com>
2021-06-17 10:45:47 -0600

    server/index.js finished

--- src/server/index.js
+++ src/server/index.js
@@ -4,25 +4,53 @@ dotenv.config();
 const mcAPIKey = process.env.API_KEY;
 console.log(`Meaning Cloud API Key = ${mcAPIKey}`);
 
-var path = require("path");
+const path = require("path");
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const mockAPIResponse = require("./mockAPI.js");
 
+let json = {
+  "title": "json test response",
+  "message": "test message",
+  "time": "current time"
+}
+
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
 app.use(express.static("dist"));
+const port = 8555;
+
+console.log(JSON.stringify(mockAPIResponse));
 
 console.log(__dirname);
 
+app.post('/input', async (req, res) => {
+  console.log('req====+>', req.body)
+  const result = await fetch(`https://api.meaningcloud.com/sentiment-2.1?key=${process.env.API_KEY}&url=${req.body.url}&lang=en`)
+  try {
+    console.log(result);
+    const response = await result.json();
+    res.send(response)
+    console.log(response);
+
+  } catch (error) {
+console.log("error", error);
+  }
+});
+
 app.get("/", function (req, res) {
-  //   res.sendFile("dist/index.html");
-  res.sendFile(path.resolve("src/client/views/index.html"));
+    res.sendFile("dist/index.html");
+  // res.sendFile(path.resolve("src/client/views/index.html"));
 });
 
 // designates what port the app will listen to for incoming requests
-app.listen(8081, function () {
-  console.log("Example app listening on port 8081!");
-});
+const server = app.listen(port, () => console.log(`Server listening on localhost:${port}`));
 
 app.get("/test", function (req, res) {
   res.send(mockAPIResponse);
