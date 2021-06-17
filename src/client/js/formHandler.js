42546f0854336ed39fbadc85549da4facf0f3122
Max Girteit <icufishmg@gmail.com>
2021-06-17 11:33:49 -0600

    form handler finished

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,16 +1,64 @@
+import bodyParser from "body-parser";
+
+const APIBaseURL = "https://api.meaningcloud.com/sentiment-2.1";
+const mcAPIKey = process.env.API_KEY;
+
+const sentimentAnalysis = async inputURL => {
+    const res = await fetch(`${APIBaseURL}?key=${mcAPIKey}&lang=en&url=${inputURL}`);
+    try {
+        const data = await res.json();
+        console.log('data: ', data);
+        return data;
+
+    } catch (error) {
+        console.log('error', error);
+    }
+};
+
+const analysisResults = async (url = "http://localhost:8555/input", data = {}) => {
+    console.log(':::Form Submitted:::', data);
+    const response = await fetch("http://localhost:8555/input", {
+        method: 'POST',
+        credentials: 'same-origin',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ 'url': url })
+    });
+    try {
+        const resData = await response.json();
+        return resData;
+    } catch (error) {
+        console.log('error', error);
+    }
+};
+
 function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-    Client.checkForName(formText)
+    if(Client.checkForName(formText)) {
+        postResults(formText).then(res => {
+            document.getElementById('score-tag').innerHTML = `score_tag: ${res.score_tag}`;
+            document.getElementById('model').innerHTML = `Model: ${res.model}`;
+            document.getElementById('agreement').innerHTML = `Agreement: ${res.agreement}`;
+            document.getElementById('subjectivity').innerHTML = `Subjectivity: ${res.subjectivity}`;
+            document.getElementById('confidence').innerHTML = `Confidence: ${res.confidence}`;
+            document.getElementById('irony').innerHTML = `Irony: ${res.irony}`;
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
-}
+            })} else {
+                alert("Enter correct URL")
+            }
+    }
+
+//     console.log("::: Form Submitted :::")
+//     fetch('http://localhost:8081/test')
+//     .then(res => res.json())
+//     .then(function(res) {
+//         document.getElementById('results').innerHTML = res.message
+//     })
+// }
 
 export { handleSubmit }
--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,4 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
 const mcAPIKey = process.env.API_KEY;
@@ -11,17 +11,19 @@ const cors = require("cors");
 const mockAPIResponse = require("./mockAPI.js");
 
 let json = {
-  "title": "json test response",
-  "message": "test message",
-  "time": "current time"
-}
+  title: "json test response",
+  message: "test message",
+  time: "current time",
+};
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}))
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 app.use(express.static("dist"));
 const port = 8555;
@@ -30,29 +32,31 @@ console.log(JSON.stringify(mockAPIResponse));
 
 console.log(__dirname);
 
-app.post('/input', async (req, res) => {
-  console.log('req====+>', req.body)
-  const result = await fetch(`https://api.meaningcloud.com/sentiment-2.1?key=${process.env.API_KEY}&url=${req.body.url}&lang=en`)
+app.post("/input", async (req, res) => {
+  console.log("req====+>", req.body);
+  const result = await fetch(
+    `https://api.meaningcloud.com/sentiment-2.1?key=${process.env.API_KEY}&url=${req.body.url}&lang=en`
+  );
   try {
     console.log(result);
     const response = await result.json();
-    res.send(response)
+    res.send(response);
     console.log(response);
-
   } catch (error) {
-console.log("error", error);
+    console.log("error", error);
   }
 });
 
 app.get("/", function (req, res) {
-    res.sendFile("dist/index.html");
+  res.sendFile("dist/index.html");
   // res.sendFile(path.resolve("src/client/views/index.html"));
 });
 
 // designates what port the app will listen to for incoming requests
-const server = app.listen(port, () => console.log(`Server listening on localhost:${port}`));
+const server = app.listen(port, () =>
+  console.log(`Server listening on localhost:${port}`)
+);
 
 app.get("/test", function (req, res) {
   res.send(mockAPIResponse);
 });
-
