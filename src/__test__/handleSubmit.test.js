ce2d30f6deb3d6a10bd39aae3b9def7c990488d2
Max Girteit <icufishmg@gmail.com>
2021-06-20 19:58:09 -0600

    commit prior to refactoring handleSubmit

--- src/__test__/handleSubmit.test.js
+++ src/__test__/handleSubmit.test.js
@@ -1,10 +1,23 @@
-import { mockHandleSubmit } from "../client/js/mockFormHandler";
+import { handleSubmit } from "../client/js/formHandler";
 
-describe("API functionality", () => {
-    test('testing the mockHandleSubmit() apiCall', async () =>
-    {
-        const json = await mockHandleSubmit()
-        expect(json.confidence).toBe(84);
-        });
-});
+// describe("API functionality", () => {
+//     test('testing the mockHandleSubmit() apiCall', async () =>
+//     {
+//         const json = await mockHandleSubmit()
+//         expect(json.confidence).toBe(84);
+//         });
+// });
 
+// The describe() function takes two arguments - a string description, and a test suite as a callback function.
+// A test suite may contain one or more related tests
+describe("Testing the submit functionality", () => {
+    // The test() function has two arguments - a string description, and an actual test as a callback function.
+    test("Testing the handleSubmit() function", () => {
+           // Define the input for the function, if any, in the form of variables/array
+           const input = 'http://www.hotdogs.com';
+           // Define the expected output, if any, in the form of variables/array
+           const output = {};
+           // The expect() function, in combination with a Jest matcher, is used to check if the function produces the expected output
+           // The general syntax is `expect(myFunction(arg1, arg2, ...)).toEqual(expectedValue);`, where `toEqual()` is a matcher
+           expect(handleSubmit(input)).toEqual(output);
+})});
