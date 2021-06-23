f1d97a6db20f73ae7651ee60d2e53ecfbb94c724
Max Girteit <icufishmg@gmail.com>
2021-06-22 20:11:43 -0600

    handlesubmit factored

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -7,36 +7,40 @@ import { checkForName } from "./nameChecker";
 const handleSubmit = async event => {
   event.preventDefault();
   let formData = document.getElementById("name").value;
+  callAPI(formData);
 
 
+};
+
+const callAPI = async formData => {
 //check if valid url
-  if (checkForName(formData)) {
+if (checkForName(formData)) {
 
-    //get request with input url as param
-    const api_url = `/textanalysis/${formData}`;
-    const res = await fetch(api_url);
-    const json = await res.json()
+  //get request with input url as param
+  const api_url = `/textanalysis/${formData}`;
+  const res = await fetch(api_url);
+  const json = await res.json()
 //update UI with response object
-    document.getElementById(
-      "agreement"
-    ).innerHTML = `Agreement Score: ${json.agreement}`;
-    document.getElementById("confidence").innerHTML = `Confidence Score: ${json.confidence}`;
-    document.getElementById(
-      "irony"
-    ).innerHTML = `Irony Score: ${json.irony}`;
-    document.getElementById(
-      "model"
-    ).innerHTML = `Model Type: ${json.model}`;
-    document.getElementById(
-      "scoretag"
-    ).innerHTML = `Score Tag: ${json.score_tag}`;
+  document.getElementById(
+    "agreement"
+  ).innerHTML = `Agreement Score: ${json.agreement}`;
+  document.getElementById("confidence").innerHTML = `Confidence Score: ${json.confidence}`;
+  document.getElementById(
+    "irony"
+  ).innerHTML = `Irony Score: ${json.irony}`;
+  document.getElementById(
+    "model"
+  ).innerHTML = `Model Type: ${json.model}`;
+  document.getElementById(
+    "scoretag"
+  ).innerHTML = `Score Tag: ${json.score_tag}`;
 
 
 
-  } else {
-    //alert if url input is not valid url
-    alert("Please enter a valid URL to continue.");
-  }
+} else {
+  //alert if url input is not valid url
+  alert("Please enter a valid URL to continue.");
+}
 };
 
 
